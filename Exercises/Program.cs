1b7ea923fad9357f43925a43cb31048b6ce4f67d
Md. Sajjat Hossen <sajjat.hossen@brainstation-23.com>
2024-05-03 17:12:39 +0600

    Exercises 4.5 done

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -5,6 +5,10 @@ internal class Program {
     // Solution511.Solution511_1();
     // Solution511.Solution511_2();
     // Solution511.Solution511_3();
-    Solution511.Solution511_4();
+    // Solution511.Solution511_4();
+    // Solution54.Solution54_1();
+    // Solution54.Solution54_2();
+    // Solution54.Solution54_3();
+    Solution54.Solution54_4();
   }
 }
---
+++ Exercises/Solutions/Solution54.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Solutions {
+  internal class Solution54 {
+    public static void Solution54_1() {
+      var userInput = Console.ReadLine();
+      var length = userInput.Length;
+      var valid = true;
+      var number = 0;
+      foreach ( var chr in userInput ) {
+        if ( chr >= '0' && chr <='9') {
+          var d = chr - '0';
+          number *= 10;
+          number += d;
+        } else {
+          valid = false;
+          break;
+        }
+      }
+      if (number < 0 || number > 10) valid = false;
+      if (valid) Console.WriteLine("Valid");
+      else Console.WriteLine("Invalid");
+    }
+
+    public static void Solution54_2() {
+      var userInput1 = Console.ReadLine();
+      var userInput2 = Console.ReadLine();
+
+      var number1 = Convert.ToInt32(userInput1);
+      var number2 = Convert.ToInt32(userInput2);
+
+      var maxNumber = number1 > number2 ? number1 : number2;
+      Console.WriteLine(maxNumber);
+    }
+
+    public static void Solution54_3() {
+      Console.Write("Enter the width of the image: ");
+      var width = int.Parse(Console.ReadLine());
+      Console.Write("Enter the height of the image: ");
+      var height = int.Parse(Console.ReadLine());
+
+      if (width > height) Console.WriteLine("The image is landscape");
+      else if (height > width) Console.WriteLine("The image is rotate");
+      else Console.WriteLine("The image is square");
+    }
+
+    public static void Solution54_4() {
+      Console.Write("Please Enter the Speed Limit: ");
+      var speedLimit = int.Parse(Console.ReadLine());
+      Console.Write("Please inter the speed of a car: ");
+      var carSpeed = int.Parse(Console.ReadLine());
+      if (carSpeed < speedLimit) {
+        Console.WriteLine("OK");
+      } else {
+        var demerits = (carSpeed - speedLimit) / 5;
+        Console.WriteLine("Demerits Points: " + demerits);
+        if (demerits > 12) Console.WriteLine("License Suspended");
+      }
+    }
+  }
+}
